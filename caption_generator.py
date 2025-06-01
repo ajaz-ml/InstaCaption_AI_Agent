568854f5db48faa4156a5879509ba08f9c4074da
ajaz-ml <ajaza7939@gmail.com>
2025-06-01 12:50:33 +0530

    Switch to flan-t5-large for reliable caption generation

--- caption_generator.py
+++ caption_generator.py
@@ -3,7 +3,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-API_URL = "https://api-inference.huggingface.co/models/mistralai/Mixtral-8x7B-Instruct-v0.1"
+API_URL = "https://api-inference.huggingface.co/models/google/flan-t5-large"
+
 HF_TOKEN = os.getenv("HF_API_KEY")
 headers = {"Authorization": f"Bearer {HF_TOKEN}"}
 
